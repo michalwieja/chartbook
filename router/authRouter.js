070f3b7a83418d0374cebda8f13fc944d62099f6
mwConceptX <mwieja@yahoo.com>
2020-12-07 18:44:49 +0100

    validation

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,9 +1,21 @@
 import express from "express";
 import User from "../models/UserModel.js";
+import Joi from "joi";
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+  //validation
+
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+    email: Joi.string().min(3).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
