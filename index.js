85e80e2e072251c6fca3268ab3d356a08788b493
mwConceptX <mwieja@yahoo.com>
2020-12-07 18:07:10 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import authRouter from "./router/authRouter.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("get ok"));
+app.use("/auth", authRouter);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, () => console.log(`server at ${PORT}`)))
+  .catch((err) => console.log(err));
---
+++ router/authRouter.js
@@ -0,0 +1,21 @@
+import express from "express";
+import User from "../models/UserModel.js";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+export default router;
