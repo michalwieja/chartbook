175a6f8ebe5581dbbe3c7b33a5ca100d1871938d
mwConceptX <mwieja@yahoo.com>
2020-12-07 21:13:56 +0100

    login

--- router/authRouter.js
+++ router/authRouter.js
@@ -4,39 +4,49 @@ import Joi from "joi";
 import bcrypt from "bcrypt";
 
 const router = express.Router();
+// schema validataion
+const schema = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().min(3).required().email(),
+  password: Joi.string().min(6).required(),
+});
 
 router.post("/register", async (req, res) => {
-  //validation
-
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-    email: Joi.string().min(3).required().email(),
-    password: Joi.string().min(6).required(),
-  });
-
   const { error } = schema.validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-
   // chceck is user exists
-  const emailExists = await User.findOne({ email: req.body.email });
-  if (emailExists) return res.status(400).send("Email already taken");
-
+  const user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("Email already taken");
   //hash
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-  const user = new User({
+  const newUser = new User({
     name: req.body.name,
     email: req.body.email,
     password: hashedPassword,
   });
 
   try {
-    const savedUser = await user.save();
+    const savedUser = await newUser.save();
     res.send({ user: user._id });
   } catch (err) {
     res.status(400).send(err);
   }
+});
+
+router.post("/login", async (req, res) => {
+  //validation
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  // chceck is user exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("wrong email");
+  //hash
+  const validUser = await bcrypt.compare(req.body.password, user.password);
+  if (!validUser) return res.status(400).send("wrong pass");
+
+  res.send("logged in");
 });
 
 export default router;
